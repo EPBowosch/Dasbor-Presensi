63dbe3e23ea25d70e430ea29c175d4a6aa73033e
EPBowosch <eko.purwanto@atmi.ac.id>
2026-09-01 15:51:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ menu_items = [
     {
         "title": "MKL Instruktur",
         "sub": "Pencatatan Minus, Kompen, dan Lembur oleh instruktur",
-        "url": "https://accounts.google.com/AccountChooser?continue=https://script.google.com/a/macros/atmi.ac.id/s/AKfycbyjhCOW6svsWOtVkjtL3a1kqC2lPzQ7b8D9TMnR-LHNrAgirbwTtJEI7QzbKguuS7NJ/exec",
+        "url": "https://accounts.google.com/AccountChooser?continue=https://script.google.com/macros/s/AKfycbxJ5omhD-drxNIvYxAklYkp0ktSOxsO6-f0MNBTB_Kop2FjTs5BLR7mZ2sdllB15ys/exec",
         "bg": "#eafaf0",
         "color": "#1a7a4c",
         # ikon jam/stopwatch (Bootstrap Icons: stopwatch)
